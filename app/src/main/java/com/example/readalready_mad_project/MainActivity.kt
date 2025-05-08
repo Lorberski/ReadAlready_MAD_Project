cbbb17d1a5b38a45b8a218d1f258b072c3490735
Lorberski <lorenz.rettenwender@stud.fh-campuswien.ac.at>
2025-05-08 15:25:43 +0200

    Initial commit

---
+++ app/src/main/java/com/example/readalready_mad_project/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.readalready_mad_project
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.readalready_mad_project.ui.theme.ReadAlready_MAD_ProjectTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ReadAlready_MAD_ProjectTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ReadAlready_MAD_ProjectTheme {
+        Greeting("Android")
+    }
+}
