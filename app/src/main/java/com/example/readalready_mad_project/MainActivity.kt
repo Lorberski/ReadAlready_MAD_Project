2aafeea24c37855ff3843f250d290f50495af7ef
Lorberski <lorenz.rettenwender@stud.fh-campuswien.ac.at>
2025-05-21 14:54:41 +0200

    deleted some unused imports in MainActivity

--- app/src/main/java/com/example/readalready_mad_project/MainActivity.kt
+++ app/src/main/java/com/example/readalready_mad_project/MainActivity.kt
@@ -6,7 +6,6 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -14,7 +13,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.readalready_mad_project.ui.theme.ReadAlready_MAD_ProjectTheme
 import dagger.hilt.android.AndroidEntryPoint
-import jakarta.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
